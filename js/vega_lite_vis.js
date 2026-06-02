06bce8f582b1b346d6a58ea17a48d065474aef2f
Chenuka-ye <hhew0008@student.monash.edu>
2026-06-02 11:37:04 +1000

    Add files via upload

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,17 @@
+// Local Melbourne Data
+vegaEmbed("#map_chart", "charts/chart1_interactive_map.vg.json").catch(console.error);
+vegaEmbed("#interactive_size_map", "charts/chart2_interactive_size_map.vg.json").catch(console.error);
+vegaEmbed("#distribution_chart", "charts/chart3_seating_distribution.vg.json").catch(console.error);
+vegaEmbed("#average_seating_chart", "charts/chart4_average_seating.vg.json").catch(console.error);
+vegaEmbed("#indoor_outdoor_chart", "charts/chart5_indoor_outdoor_diverging.vg.json").catch(console.error);
+vegaEmbed("#growth_chart", "charts/chart6_seating_growth.vg.json").catch(console.error);
+
+// National Spending Data
+vegaEmbed("#spending_chart", "charts/chart7_spending_trends_annotated.vg.json").catch(console.error);
+vegaEmbed("#seasonality_heatmap", "charts/chart12_seasonality_heatmap.vg.json").catch(console.error);
+vegaEmbed("#monthly_spending_chart", "charts/chart8_monthly_spending.vg.json").catch(console.error);
+vegaEmbed("#covid_resilience_chart", "charts/chart10_covid_resilience.vg.json").catch(console.error);
+
+// Global Price Data
+vegaEmbed("#price_vs_spending_chart", "charts/chart11_price_vs_spending.vg.json").catch(console.error);
+vegaEmbed("#habits_chart", "charts/chart9_coffee_habits_lollipop.vg.json").catch(console.error);
